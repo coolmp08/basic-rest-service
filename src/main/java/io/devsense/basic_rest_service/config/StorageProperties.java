0d5a8c82e63b136097107af50e9d3cd3aeff0178
coolmp08 <coolmp08@gmail.com>
2024-10-14 18:42:37 +0530

    file-upload-intial-v2

--- src/main/java/io/devsense/basic_rest_service/config/StorageProperties.java
+++ src/main/java/io/devsense/basic_rest_service/config/StorageProperties.java
@@ -3,11 +3,13 @@ package io.devsense.basic_rest_service.config;
 import jdk.jfr.DataAmount;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
 @ConfigurationProperties("storage")
+@Component
 public class StorageProperties {
 
-    private String location ="upload-dir";
+    private String location ="/Users/manas/works/spring-guides/basic-rest-service/src/main/resources/upload-dir";
 
 }
